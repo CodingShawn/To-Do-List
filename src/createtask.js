aa28b6f0860ab9f4fb7722f56884d52370f308f7
Shawn <codingshawn@gmail.com>
2020-11-07 23:13:50 +0800

    Did up basic createtask factory function

---
+++ src/createtask.js
@@ -0,0 +1,18 @@
+const createTask = (title, description, dueDate, priority) => {
+    let taskDiv = document.createElement("div");
+    taskDiv.classList.add("task-wrapper")
+    let taskHeader = document.createElement("div");
+    taskHeader.classList.add("task-header")
+    let taskTitle = document.createElement("h3");
+    taskTitle.textContent = title;
+    let taskDueDate = document.createElement("p");
+    taskDueDate.textContent = "Due: " + dueDate;
+    taskHeader.appendChild(taskTitle);
+    taskHeader.appendChild(taskDueDate);
+
+    taskDiv.appendChild(taskHeader);
+
+    return taskDiv;
+}
+
+export default createTask;
