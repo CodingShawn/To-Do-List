bffe3c973d235d71eef525a8c61dbed04d43a935
Shawn <codingshawn@gmail.com>
2020-11-09 22:05:57 +0800

    Added functionality to show/hide task descriptions as well as did styling of tasks items

--- src/createtask.js
+++ src/createtask.js
@@ -3,6 +3,7 @@ import pubsub from "./pubsub.js";
 const { default: dom } = require("./dom.js")
 
 const createTask = (title, description, dueDate, priority) => {
+    //Create main task elements
     let taskDiv = document.createElement("div");
     taskDiv.classList.add("task-wrapper")
     let taskHeader = document.createElement("div");
@@ -13,10 +14,29 @@ const createTask = (title, description, dueDate, priority) => {
     taskDueDate.textContent = "Due: " + dueDate;
     taskHeader.appendChild(taskTitle);
     taskHeader.appendChild(taskDueDate);
-
     taskDiv.appendChild(taskHeader);
 
+    //Add task description
+    let taskBody = document.createElement("div");
+    taskBody.classList.add('task-body');
+
+    let taskPriority = document.createElement("div");
+    taskPriority.textContent = "Priority: " + priority;
+    taskPriority.classList.add("priority");
+    let taskDescription = document.createElement("div");
+    taskDescription.textContent = description;
+    taskBody.appendChild(taskPriority);
+    taskBody.appendChild(taskDescription);
+
+    taskDiv.appendChild(taskBody);
+
     pubsub.publish('addTaskToProject', taskDiv);
+
+    //Allow taskbody to be hidden
+    taskDiv.addEventListener('click', () => {
+        taskBody.classList.contains('hidden') ? taskBody.classList.remove('hidden') :
+            taskBody.classList.add('hidden')
+    })
 }
 
 export default createTask;
