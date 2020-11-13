e94f4821d4d19e3a78fa8d23d68973c3bfa1ec1d
Shawn <codingshawn@gmail.com>
2020-11-13 22:03:51 +0800

    Added icons for controller task actions

--- src/createtask.js
+++ src/createtask.js
@@ -16,17 +16,45 @@ const createTask = (title, description, dueDate, priority) => {
     taskHeader.appendChild(taskDueDate);
     taskDiv.appendChild(taskHeader);
 
-    //Add task description
+    //Add task priority
     let taskBody = document.createElement("div");
     taskBody.classList.add('task-body');
     taskBody.classList.add('hidden');
 
+    let taskBodyHeader = document.createElement("div");
+    taskBodyHeader.classList.add('task-body-header');
+
     let taskPriority = document.createElement("div");
     taskPriority.textContent = "Priority: " + priority;
     taskPriority.classList.add("priority");
+    taskBodyHeader.appendChild(taskPriority);
+
+    //Add icons for edit/delete/complete
+    let iconWrapper = document.createElement("div");
+    iconWrapper.classList.add('icon-wrapper');
+
+    let completedIcon = document.createElement("span");
+    completedIcon.classList.add('material-icons');
+    completedIcon.textContent = "check_circle";
+    iconWrapper.appendChild(completedIcon);
+
+    let editIcon = document.createElement("span");
+    editIcon.classList.add('material-icons');
+    editIcon.textContent = "create";
+    iconWrapper.appendChild(editIcon);
+
+    let deleteIcon = document.createElement("span");
+    deleteIcon.classList.add('material-icons');
+    deleteIcon.textContent = "delete";
+    iconWrapper.appendChild(deleteIcon); 
+
+    taskBodyHeader.appendChild(iconWrapper);
+    taskBody.appendChild(taskBodyHeader);
+
+    //Add task description
+    
     let taskDescription = document.createElement("div");
     taskDescription.textContent = description;
-    taskBody.appendChild(taskPriority);
     taskBody.appendChild(taskDescription);
 
     taskDiv.appendChild(taskBody);
