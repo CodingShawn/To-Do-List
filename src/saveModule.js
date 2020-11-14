80cb8e07231833d7524f990f4bdc6c95ee17929e
Shawn <codingshawn@gmail.com>
2020-11-14 19:15:58 +0800

    Added saving via local storage function

---
+++ src/saveModule.js
@@ -0,0 +1,28 @@
+import dom from './dom.js'
+import pubsub from './pubsub'
+
+const saveModule = ((projectController) => {
+    function updateStorage() {
+        localStorage.setItem('to-do-list', JSON.stringify(projectController.projects));
+    }
+    
+    function loadStorage(){
+        if (localStorage.getItem('to-do-list')){
+            let toLoadProjects = JSON.parse(localStorage.getItem('to-do-list'));
+            for (let project of toLoadProjects) {
+                let currentLoadingProject = projectController.createProject(project.projectName);
+                console.log(currentLoadingProject)
+                dom.setTargetedProject(currentLoadingProject.projectDetailsWrapper);
+                for (let task of project.childObjects) {
+                    pubsub.publish('loadTask', task);
+                }
+            }
+        }
+    }
+    
+    window.addEventListener('load', loadStorage);
+    window.addEventListener('unload', updateStorage);
+});
+
+export default saveModule;
+
